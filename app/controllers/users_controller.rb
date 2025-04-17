60ec2b29fb1c9a11eec21500f27aee5f7da9660b
Jacob Sanders <sanders.jms@gmail.com>
2025-04-16 20:30:16 -0600

    built edit/update view and functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
@@ -12,6 +16,16 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
